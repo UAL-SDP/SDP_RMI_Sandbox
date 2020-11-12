8f16d5ba23c126a3822fb0db24c27cb4c3d637a7
amgs <undisclosed>
2020-11-12 13:23:40 +0000

    Update server program.

--- src/ServerProgram.java
+++ src/ServerProgram.java
@@ -4,6 +4,7 @@ import java.rmi.RemoteException;
 
 public class ServerProgram {
     public static void main(String[] args) {
+        System.out.println("[Server started]");
         try {
             Naming.rebind("MathServer1", new MathServerClass());
             // rmi://127.0.0.1/MathServer1
